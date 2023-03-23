a4ff9f83798fee0089714675572231cd042eda19
Sam <samara.amanda01@gmail.com>
2023-03-23 02:24:33 -0400

    project npm setup installations

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                
+                <a className="navbar-brand mb-1 h1" id="nav-brand-name" href="#">
+
+                    <img src="../brand-2.png" className="d-inline-block" />
+
+                    RENTOPIA
+                    <span className="navbar-brand" id="nav-sub-brand">LUXURY REALTY</span>
+                </a>
+                
+            
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Add Listing</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
