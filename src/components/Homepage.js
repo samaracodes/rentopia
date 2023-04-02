7dc975336cfaf2b3451d0d9a577071e4c102f5b2
Sam <samaracodes@gmail.com>
2023-04-01 21:36:22 -0400

    homepage styling, navbar link addition

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -28,20 +28,38 @@ const Homepage = ({ listings }) => {
 
     return (
         <>
-            <h1 className="display-3" id="greeting-heading">
+            {/* <h1 className="display-3" id="greeting-heading">
                 Welcome to Rentopia
-            </h1>
+            </h1> */}
 
-            <div id="homepage-carousel" class="carousel slide" data-bs-ride="carousel">
+            <div id="homepage-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-pause="false">
                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="./rental-carousel1.jpeg" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item">
-                    <img src="./rental-carousel2.jpg" class="d-block w-100" alt="..."/>
-                </div>
-                <div class="carousel-item">
-                    <img src="./rental-carousel5.jpg" class="d-block w-100" alt="..."/>
+
+                    <div class="carousel-item active" data-bs-interval="2000">
+                        <img src="./rental-carousel1.jpeg" class="d-block w-100" alt="..."/>
+                    </div>
+
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="./rental-carousel2.jpg" class="d-block w-100" alt="..."/>
+                    </div>
+
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="./rental-carousel3.jpg" class="d-block w-100" alt="..."/>
+                    </div>
+
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="./rental-carousel4.jpg" class="d-block w-100" alt="..."/>
+                    </div>
+
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="./rental-carousel5.jpg" class="d-block w-100" alt="..."/>
+                    </div>
+
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="./rental-carousel6.jpg" class="d-block w-100" alt="..."/>
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="./rental-carousel7.jpg" class="d-block w-100" alt="..."/>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,31 +2,35 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                
-                <a className="navbar-brand mb-1 h1" id="nav-brand-name" href="/">
+        <>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    
+                    <a className="navbar-brand mb-1 h1" id="nav-brand-name" href="/">
+                        <img src="../brand-2.png" className="d-inline-block" />
 
-                    <img src="../brand-2.png" className="d-inline-block" />
+                        RENTOPIA
+                        <span className="navbar-brand" id="nav-sub-brand">LUXURY RENTALS</span>
+                    </a>
+                    
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/" alt="homepage nav link">Home</a>
+                            </li>
 
-                    RENTOPIA
-                    <span className="navbar-brand" id="nav-sub-brand">LUXURY RENTALS</span>
-                </a>
-                
-            
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
-                        </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/listings/new" alt="add listing nav link">Add Listing</a>
+                            </li>
 
-                        <li className="nav-item">
-                            <a className="nav-link" href="/projects/new">Add Listing</a>
-                        </li>
-                    </ul>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/favorites" alt="listing favorites">Favorites </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     )
 }
 
