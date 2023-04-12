8f41b45e3f618cc6046704b88b002a411b19bac3
Sam <samaracodes@gmail.com>
2023-04-12 18:53:34 -0400

    set listing state as null, conditional rendering if listing isLoaded, fetch req for individual listing

--- src/components/ListingItem.js
+++ src/components/ListingItem.js
@@ -1,18 +1,46 @@
 import React, { useEffect, useState }from "react";
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const ListingItem = () => {
     const { id } = useParams() 
-    const [listing, setListing] = useState({})
+    const [listing, setListing] = useState(null)
+    const [ isLoaded, setIsLoaded ] = useState(false)
 
 
-    console.log(id)
+    useEffect(() => {
+        fetch(`http://localhost:3001/listings/${id}`)
+        .then((resp) => resp.json())
+        .then((listing => {
+            // Currently, before we can get to this step, we need to be able to destructure project
+            setListing(listing)
+
+            // Invoke setIsLoaded to change isLoaded
+            setIsLoaded(!isLoaded)
+        }))
+    }, [id])
+
+
+    //Reference isLoaded State. If false, render simple "Hi.. Loading.." component
+    if (!isLoaded) return <h1>Loading...</h1>
+
+    const { imageUrl, location, price, bedrooms, bathrooms, listingDescription } = listing 
     
     return (
         <>
-        <h1>Listing Item</h1>
-         
         <h1>Listing Id: {id}</h1>
+
+        <img src={imageUrl} alt="Listing Images" style={{width: 800, height: 500}}  />
+        <h1>{price}</h1>
+        <h3>Location: {location}</h3>
+        <h3>Bedroooms: {bedrooms}</h3>
+        <h3>Bathrooms: {bathrooms}</h3>
+        <p>{listingDescription}</p>
+
+
+        <Link to={`/listings/${id}/edit`} className="btn btn-primary">Edit</Link>
+        <br/>
+
+        <Link className="btn btn-danger">Delete</Link>
         </>
     )
 }
