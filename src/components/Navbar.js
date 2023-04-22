aa8a9ecf2b114b8d8c38e29b137b1e4540cecf59
Sam <samaracodes@gmail.com>
2023-04-21 23:01:20 -0400

    navbar link changed to react navlink components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -6,7 +7,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-sm">
                 <div className="container-fluid">
                     
-                    <a className="navbar-brand mb-1 h1" id="nav-brand-name" href="/">
+                    <a className="navbar-brand mb-1 h1" id="nav-brand-name" href="/rentopia">
                         <img src="./brand-2.png" className="d-inline-block" />
 
                         RENTOPIA
@@ -16,19 +17,19 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/rentopia" alt="homepage nav link">Home</a>
+                                <NavLink className="nav-link active" aria-current="page" to="/" alt="homepage nav link">Home</NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="/listings" alt="all available rentals nav link">Available Rentals</a>
+                                <NavLink className="nav-link" to="/listings" alt="all available rentals nav link">Available Rentals</NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="/listings/new" alt="add listing nav link">Add Listing</a>
+                                <NavLink className="nav-link" to="/listings/new" alt="add listing nav link">Add Listing</NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <a className="nav-link" href="/favorites" alt="listing favorites">Favorites </a>
+                                <NavLink className="nav-link" to="/favorites" alt="listing favorites">Favorites </NavLink>
                             </li>
                         </ul>
                     </div>
